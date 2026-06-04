af71c9b4a7cf74566997a0f4cc35f0fda9109be0
Eudy De Oleo <eudydeoleo@icloud.com>
2026-06-04 16:08:40 -0400

    add mobile menu

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
       {/* NAVBAR */}
 
       <nav className="navbar">
-        <div src="/logo.png"></div>
+        <div className="logo">ED</div>
 
         <div className="menu">
           <a href="#skills">Skills</a>
